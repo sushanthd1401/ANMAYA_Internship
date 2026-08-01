cbbc367b6f1c1a6418116fa53d72e4667ab38f5a
Sushanth.D <sushanthd1451@gmail.com>
2026-08-01 18:33:24 +0530

    initial commit

---
+++ DAY4/CODE/half_adder.v
@@ -0,0 +1,7 @@
+module half_adder(
+    input a, input b,
+    output sum, output carry
+);
+    xor(sum, a, b);
+    and(carry, a, b);
+endmodule
---
+++ DAY4/CODE/half_adder_tb.v
@@ -0,0 +1,30 @@
+module half_adder_tb;
+
+   
+    reg A;
+    reg B;
+
+   
+    wire Sum;
+    wire Carry;
+
+    
+    half_adder uut (
+        .A(A),
+        .B(B),
+        .Sum(Sum),
+        .Carry(Carry)
+    );
+
+    
+    initial begin
+        A = 0; B = 0; #10;
+        A = 0; B = 1; #10;
+        A = 1; B = 0; #10;
+        A = 1; B = 1; #10;
+       
+
+        $finish;
+    end
+
+endmodule
